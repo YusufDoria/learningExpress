8eb5889db200bd387d7e443da90651608a97bb0a
Yusuf Doria <yusufdoria123@gmail.com>
2022-11-07 12:21:31 -0500

    11/07/22

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
-const _ = require('lodash');
+const {readFile} = require('fs');
 
-const items = [1, [2, [3, [4]]]];
-const newItems = _.flattenDeep(items);
+const getText = (path) => {
+     return new Promise((resolve, reject) => {
+          readFile(path, 'utf8', (err, result) => {
+               if (err) {
+                    reject(err);
+               }
+               else {
+                    resolve(result);
+               }
+          }) 
+     })
+}
 
-console.log(newItems);
+
+// getText('./content/first.txt').then((result) => {console.log(result)}).catch((err) => console.log(err));
+
+async function start() {
+     try{
+          const first = await getText('./content/first.txt');
+          const second = await getText('./content/second.txt'); 
+          console.log(first, second);
+     }
+     catch (error) {
+          console.log(error);
+     }  
+}
+
+start();
+
