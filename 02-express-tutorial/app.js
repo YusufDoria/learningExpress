f8a78d4ca3a10b7bbd5766885670c86a284a01f5
Yusuf Doria <yusufdoria123@gmail.com>
2022-11-09 19:30:38 -0500

    11/09/22

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,2 +1,17 @@
-console.log('Express Tutorial')
+const express = require('express');
+const app = express();
 
+app.get('/', (req,res) => {
+    res.send('home');
+})
+
+app.get('/about', (req,res) => {
+    res.send('about page');
+})
+
+
+
+
+app.listen(8000, () => {
+    console.log('Server is up');
+})
