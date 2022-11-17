6b74fdedda3ab8249ff750db2350588bce102724
Yusuf Doria <yusufdoria123@gmail.com>
2022-11-16 22:07:41 -0500

    11/16/22

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,17 +1,35 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
+let { people } = require('./data');
 
-app.get('/', (req,res) => {
-    res.send('home');
+
+app.use(express.static('./methods-public'));
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json());
+
+
+
+app.get('/api/people', (req,res) => {
+    res.status(200).json({success:true, data:people})
 })
 
-app.get('/about', (req,res) => {
-    res.send('about page');
+app.post('/api/people', (req,res) => {
+    const {name} = req.body;
+    if(!name){
+        return res.status(400).json({success:false,msg:"please provide name"});
+    }
+    res.status(201).json({success:true,person:name});
 })
 
-
-
+app.post('/login', (req,res) => {
+    const {name} = req.body;
+    if(name) {
+        return res.status(200).send(`welcome ${name}`);
+    }
+    res.status(401).send('please provide a name');
+})
 
 app.listen(8000, () => {
-    console.log('Server is up');
+  console.log('Server is listening on port 8000....')
 })
